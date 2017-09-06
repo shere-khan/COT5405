26c87b6b43609ff4534271151c21e23c319f6668
Justin-PC\Justin <justin.barry.e>
2017-09-06 17:41:06 -0400

    Revert "Need to prob change preferences dict back to list"
    This reverts commit 716cef4

--- homework1/__init__.py
+++ homework1/__init__.py
@@ -12,9 +12,4 @@ if __name__ == '__main__':
     men = []
     sm.Game.create_players(men, women, mens_stack, womens_stack)
     sm.Game.set_preferences(men, women, mens_names, womens_names)
-    # sm.Game.set_preferences(men, women)
-
-    # for m in men:
-    #     print(m.preferences_map)
-
-    sm.Game.gale_shapley(men)
+    sm.Game.gale_shapley(men, women)
