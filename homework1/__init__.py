ae51ab5f577c58db9b651ef4fc4b0858329c25e5
Justin-PC\Justin <justin.barry.e>
2017-09-06 00:13:03 -0400

    Initialized men and women with their preferences

---
+++ homework1/__init__.py
@@ -0,0 +1,15 @@
+from homework1 import stable_marriage as sm
+from copy import deepcopy
+
+if __name__ == '__main__':
+    SIZE = 5
+    womens_stack = ['suzy', 'katrina', 'felicia', 'jenae', 'kyana']
+    womens_names = deepcopy(womens_stack)
+    mens_stack = ['germaine', 'michael', 'andre', 'justin', 'tim']
+    mens_names = deepcopy(mens_stack)
+
+    women = []
+    men = []
+    sm.Game.create_players(men, women, mens_stack, womens_stack)
+
+    sm.Game.set_preferences(men, women, mens_names, womens_names)
--- homework1/stable_marriage.py
+++ homework1/stable_marriage.py
@@ -1,13 +1,46 @@
+from random import shuffle, randint
+from copy import deepcopy
+
+from homework1 import stable_marriage as sm
+
+
 class Person:
-    def __init__(self):
+    def __init__(self, name):
         self.preference_list = []
+        self.name = name
+        self.partner = None
+
+    def __str__(self):
+        return "{0} {1}".format(self.name, self.partner)
+
+    def __repr__(self):
+        return "{0} {1}".format(self.name, self.partner)
 
 
 class Man(Person):
-    def __init__(self):
-        Person.__init__(self)
+    def __init__(self, name):
+        Person.__init__(self, name)
 
 
 class Woman(Person):
+    def __init__(self, name):
+        Person.__init__(self, name)
+
+
+class Game:
     def __init__(self):
-        Person.__init__(self)
+        pass
+
+    @staticmethod
+    def create_players(men, women, mens_names, womens_names):
+        while womens_names and mens_names:
+            women.append(sm.Woman(womens_names.pop(randint(0, len(womens_names) - 1))))
+            men.append(sm.Man(mens_names.pop(randint(0, len(mens_names) - 1))))
+
+    @staticmethod
+    def set_preferences(men, women, mens_names, womens_names):
+        for m, w in zip(men, women):
+            shuffle([womens_names])
+            m.preference_list = deepcopy(womens_names)
+            shuffle([mens_names])
+            w.preference_list = deepcopy(mens_names)
