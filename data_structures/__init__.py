01824e6ec84d3524251c70822ae85edacfd31835
Justin-PC\Justin <justin.barry.e>
2017-09-09 16:14:55 -0400

    Impl'ing alg to populate undirected graph

---
+++ data_structures/__init__.py
@@ -0,0 +1,5 @@
+from data_structures import graph as g
+
+if __name__ == '__main__':
+    graph = g.UndirectedGraph()
+    g.GraphData.populate_graph(graph, 4, 20)
--- data_structures/graph.py
+++ data_structures/graph.py
@@ -1,3 +1,18 @@
+import random
+
+
+class Node:
+    def __init__(self):
+        self.__value = ""
+        self.discovered = False
+
+    def set_value(self, value):
+        self.__value = value
+
+    def get_value(self):
+        return self.__value
+
+
 class UndirectedGraph:
     def __init__(self):
         self.__adj_dict = dict()
@@ -8,13 +23,66 @@ class UndirectedGraph:
     def set_adj_dict(self, adj_dict):
         self.__adj_dict = adj_dict
 
+    def add_node(self, node, **kwargs):
+        self.__adj_dict[node] = None
+        for name, value in kwargs.items():
+            if name is 'neighbors':
+                neighbors = name
+                self.__adj_dict[node] = neighbors
+                for n in neighbors:
+                    self.__adj_dict[n] = node
 
-class Node:
-    def __init__(self):
-        self.__value = ""
+    def add_connection(self, node, neighbor):
+        self.__adj_dict[node].append(neighbor)
 
-    def set_value(self, value):
-        self.__value = value
+    def size(self):
+        n = 0
+        for node, adj_list in self.__adj_dict.items():
+            if adj_list:
+                n += len(adj_list)
 
-    def get_value(self):
-        return self.__value
+        n += len(self.__adj_dict)
+        return n
+
+    @staticmethod
+    def breadth_first_traversal(node):
+        node.discovered = True
+        to_visit_queue = list(node)
+        while to_visit_queue:
+            u = to_visit_queue.pop(0)
+            neighbors = u.__adj_dict[node]
+            for v in neighbors:
+                if v.discovered is False:
+                    v.discovered = True
+                    to_visit_queue.append(v)
+
+
+class GraphData:
+    # TODO implement interval_size_lim
+    @staticmethod
+    def populate_graph(graph, interval_size_lim, total_size):
+        # Add a node by default
+        first_node = Node()
+
+        # If the graph has not yet been populated, add another node
+        # and connect it to the first node
+        graph.add_node(first_node)
+        if graph.size() is 1:
+            graph.add_node(Node(), first_node)
+
+        # Start populating the rest of the graph
+        while graph.size() < total_size:
+            limit = 0
+            new_node = Node()
+            graph.add_node(Node(), new_node)
+            while limit < interval_size_lim:
+                neighb_key = random.sample(list(graph.get_adj_dict()), 1)
+                neigb_list = graph.get_adj_dict()[neighb_key]
+                neighb = random.sample(neigb_list, 1)
+                # Check and see if node is the node we just added
+                if neighb is not new_node:
+                    pass
+                limit += 1
+                if limit is interval_size_lim:
+                    break
+            # graph.add_node(Node(), neighbors)
