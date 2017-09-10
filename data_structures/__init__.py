8fe5af70310d42b4c39ff189594ad689379c639a
Justin-PC\Justin <justin.barry.e>
2017-09-09 23:20:37 -0400

    fininshed BFS

--- data_structures/__init__.py
+++ data_structures/__init__.py
@@ -2,4 +2,5 @@ from data_structures import graph as g
 
 if __name__ == '__main__':
     graph = g.UndirectedGraph()
-    g.GraphData.populate_graph(graph, 4, 20)
+    g.GraphData.populate_graph(graph, 2, 20)
+    g.GraphData.breadth_first_traversal(graph, graph.get_random_node(), lambda x: print(x.get_value()))
--- data_structures/graph.py
+++ data_structures/graph.py
@@ -23,17 +23,14 @@ class UndirectedGraph:
     def set_adj_dict(self, adj_dict):
         self.__adj_dict = adj_dict
 
-    def add_node(self, node, **kwargs):
-        self.__adj_dict[node] = None
-        for name, value in kwargs.items():
-            if name is 'neighbors':
-                neighbors = name
-                self.__adj_dict[node] = neighbors
-                for n in neighbors:
-                    self.__adj_dict[n] = node
+    def add_node(self, node):
+        # If only node parameter, add the node to the dict
+        # with a None value
+        self.__adj_dict[node] = list()
 
     def add_connection(self, node, neighbor):
         self.__adj_dict[node].append(neighbor)
+        self.__adj_dict[neighbor].append(node)
 
     def size(self):
         n = 0
@@ -42,50 +39,74 @@ class UndirectedGraph:
                 n += len(adj_list)
 
         n += len(self.__adj_dict)
+
         return n
 
-    @staticmethod
-    def breadth_first_traversal(node):
-        node.discovered = True
-        to_visit_queue = list(node)
-        while to_visit_queue:
-            u = to_visit_queue.pop(0)
-            neighbors = u.__adj_dict[node]
-            for v in neighbors:
-                if v.discovered is False:
-                    v.discovered = True
-                    to_visit_queue.append(v)
+    def get_random_node(self):
+        node_key = random.sample(list(self.get_adj_dict()), 1)[0]
+        node_list = self.get_adj_dict()[node_key]
+        node = random.sample(node_list, 1)[0]
+
+        return node
 
 
 class GraphData:
-    # TODO implement interval_size_lim
+    # TODO implement interval_size_lim range
     @staticmethod
     def populate_graph(graph, interval_size_lim, total_size):
         # Add a node by default
         first_node = Node()
+        node_value = 0
+        first_node.set_value(node_value)
+        node_value += 1
 
         # If the graph has not yet been populated, add another node
         # and connect it to the first node
         graph.add_node(first_node)
         if graph.size() is 1:
-            graph.add_node(Node(), first_node)
+            second_node = Node()
+            second_node.set_value(node_value)
+            node_value += 1
+
+            graph.add_node(second_node)
+            graph.add_connection(first_node, second_node)
 
         # Start populating the rest of the graph
         while graph.size() < total_size:
             limit = 0
             new_node = Node()
-            graph.add_node(Node(), new_node)
+            new_node.set_value(node_value)
+            node_value += 1
+            graph.add_node(new_node)
             while limit < interval_size_lim:
-                neighb_key = random.sample(list(graph.get_adj_dict()), 1)
+                # Get random key (node) from adjacency dict
+                neighb_key = random.sample(list(graph.get_adj_dict()), 1)[0]
+                # Get the adjacency list corresponding to the key
                 neighb_list = graph.get_adj_dict()[neighb_key]
-                neighb = random.sample(neighb_list, 1)
+                if neighb_list is not None and len(neighb_list) > 0:
+                    neighb = random.sample(neighb_list, 1)[0]
+                    # Get the adjacency list for the new node
+                    new_node_neighbor_list = graph.get_adj_dict()[new_node]
+                    # If neighb is not the node we just added make it a connection of new_node
+                    if neighb is not new_node and neighb not in new_node_neighbor_list:
+                            graph.add_connection(new_node, neighb)
+                            limit += 1
 
-                # If neighb is not the node we just added
-                # make it a connection of new_node
-                if neighb is not new_node:
-                    graph.add_connection(new_node, neighb)
-                    limit += 1
+                            if limit is interval_size_lim:
+                                break
 
-                    if limit is interval_size_lim:
-                        break
-            #
+    @staticmethod
+    def breadth_first_traversal(graph, node, f):
+        # f(node)
+        print(node.get_value())
+        node.discovered = True
+        to_visit_queue = [node]
+        while to_visit_queue:
+            u = to_visit_queue.pop(0)
+            neighbors = graph.get_adj_dict()[u]
+            for v in neighbors:
+                if v.discovered is False:
+                    # f(node)
+                    print(v.get_value())
+                    v.discovered = True
+                    to_visit_queue.append(v)
